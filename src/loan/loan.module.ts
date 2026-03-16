bdfecae679d29c3c75065a621ee83dbec06ae524
Александр Уткин <a.utkin@utplatform.ru>
2026-03-16 14:52:10 +0300

    LP-work: add loan repo

--- src/loan/loan.module.ts
+++ src/loan/loan.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoanService } from './loan.service';
+import { Loan } from './loan.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Loan])],
   providers: [LoanService],
   exports: [LoanService], // Export LoanService if it will be used by other modules
 })
